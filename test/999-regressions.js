e8241abd093e5f71970bfcb6b019b86cecd6c3d8
Adam Brady <adam@boxxen.org>
2016-06-05 11:17:45 +1000

    add failing regession test

---
+++ test/999-regressions.js
@@ -0,0 +1,19 @@
+var assert = require('assert')
+
+var compiler = require('../compiler')
+
+describe('regressions', function() {
+
+  it('should compile functions', function () {
+    var fn = 'def myFn [ one two ] (add one two)'
+    var out = 'function myFn(one, two) {\n    return add(one, two);\n}'
+    assert.equal(compiler(fn), out)
+  })
+
+  it('should compile inline function calls', function () {
+    var fn = '(add 5 (subtract 5 1))'
+    var out = 'add(5, subtract(5, 1));'
+    assert.equal(compiler(fn), out)
+  })
+
+})
